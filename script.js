ef97059c73e4edca4153957352fc831cf2ee6c1c
danielz0102 <danyzglez@hotmail.com>
2024-12-10 19:58:46 -0600

    Show books in a table

--- script.js
+++ script.js
@@ -5,17 +5,45 @@ function Book(title, author, pages, readed) {
   this.author = author
   this.pages = pages
   this.readed = readed
-
-  this.info = () => {
-    const readedMsg = readed ? 'already read' : 'not read yet'
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readedMsg}`
-  }
 }
 
-function addBookToLibrary(title, author, pages, readed) {
-  const newBook = new Book(title, author, pages, readed)
+Book.prototype.info = function() {
+  const readedMsg = readed ? 'already read' : 'not read yet'
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readedMsg}`
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read)
   library.push(newBook)
 }
 
-addBookToLibrary('The Hobbit', 'Tolkien', '295', false)
-console.log(library)
+function showBooks() {
+  library.forEach(book => {
+    const body = document.querySelector('#libraryTable tbody')
+    const row = document.createElement('tr')
+
+    for (const key in book) {
+      if (Object.prototype.hasOwnProperty.call(book, key)) {
+        let value = book[key]
+
+        if (typeof value === 'boolean') {
+          value = value ? '✅' : '❌'
+        }
+
+        const cell = document.createElement('td')
+        cell.textContent = value
+        row.appendChild(cell)
+      }
+    }
+
+    body.appendChild(row)
+  })
+}
+
+addBookToLibrary('1984', 'George Orwell', '328', true);
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', '281', false);
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', '180', true);
+addBookToLibrary('Pride and Prejudice', 'Jane Austen', '279', false);
+addBookToLibrary('Moby Dick', 'Herman Melville', '635', true);
+
+showBooks()
